d13f0c5427e8838ec221d7020608434f6ec438ec
Pierre <p.dellanave@gmail.com>
2011-03-30 01:34:51 -0700

    code cleanup - devmode to 0

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -18,7 +18,7 @@ def MainMenu():
   for item in XML.ElementFromURL(RSS_FEED).xpath("//item"):
     title = item.xpath('.//title')[0].text
     url = item.xpath('.//link')[0].text
-    raw_summary = HTML.StringFromElement(item.xpath('.//description')[0]).replace(']]','').replace('<![CDATA[','').replace('&gt;','>').replace('&lt;','<')#.text
+    raw_summary = HTML.StringFromElement(item.xpath('.//description')[0]).replace(']]','').replace('<![CDATA[','').replace('&gt;','>').replace('&lt;','<').replace('&nbsp;','')#.text
     summary = HTML.ElementFromString(raw_summary).text_content()
     try:
       thumb = HTML.ElementFromString(raw_summary).xpath('.//img')[0].get('src')
